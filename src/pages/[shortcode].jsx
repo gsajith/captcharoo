25b93368aa8f62190cb6ce6a8974f5563779e843
Gautham Sajith <gsajith@umich.edu>
2023-03-29 01:50:59 -0400

    This works for now re SSR

--- src/pages/[shortcode].jsx
+++ src/pages/[shortcode].jsx
@@ -6,7 +6,6 @@ import ReCAPTCHA from "react-google-recaptcha";
 import { testCaptcha } from "../utils";
 
 const CaptchaPage = (props) => {
-  console.log(props);
   const router = useRouter();
   const { shortcode } = router.query;
 
@@ -97,25 +96,19 @@ export const getServerSideProps = async ({ params }) => {
       }),
     };
 
-    console.log("here");
     const response = await fetch(endpoint, options);
-    console.log("here2");
     const result = await response.json();
-    console.log("here3");
 
     if (response.ok) {
-      console.log("here4");
       return {
         props: { name: result.data.name },
       };
     } else {
-      console.log("here5");
       return {
         props: {},
       };
     }
   } catch (error) {
-    console.log("here6", error);
     return {
       props: {},
     };
