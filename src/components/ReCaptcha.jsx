ae6f7a854c39e96653cc71ea6801d1e7f4191f49
Gautham Sajith <gsajith@umich.edu>
2023-04-04 01:01:42 -0400

    Recaptcha placeholder to prevent popin

--- src/components/ReCaptcha.jsx
+++ src/components/ReCaptcha.jsx
@@ -1,15 +1,38 @@
 import ReCAPTCHA from "react-google-recaptcha";
 import { testCaptcha } from "../utils";
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import { useState } from "react";
 
 const ReCaptcha = ({ setSolved }) => {
-  if (process.env.NODE_ENV === "development") {
-    return <button onClick={() => setSolved(true)}>Bypass Captcha</button>;
-  }
+  const [captchaLoading, setCaptchaLoading] = useState(true);
+  // if (process.env.NODE_ENV === "development") {
+  //   return <button onClick={() => setSolved(true)}>Bypass Captcha</button>;
+  // }
   return (
-    <ReCAPTCHA
-      sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-      onChange={(code) => testCaptcha(code, () => setSolved(true))}
-    />
+    <div className={styles.captchaContainer}>
+      {captchaLoading && (
+        <Image
+          src="/recaptcha_placeholder.png"
+          alt="Captcha"
+          fill="true"
+          style={{
+            objectFit: "cover",
+            objectPosition: "center",
+            position: "absolute",
+            top: 0,
+            zIndex: 0,
+          }}
+        />
+      )}
+      <ReCAPTCHA
+        asyncScriptOnLoad={() =>
+          setTimeout(() => setCaptchaLoading(false), 250)
+        }
+        sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+        onChange={(code) => testCaptcha(code, () => setSolved(true))}
+      />
+    </div>
   );
 };
 
