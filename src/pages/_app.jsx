a9b9d8644dce457bb90b85f9bf445bb8bbc0bc06
Gautham Sajith <gsajith@umich.edu>
2023-04-03 20:42:20 -0400

    Link preview meta tags

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,47 @@
+import Head from "next/head";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Captcharoo — Bot or not?</title>
+        <meta name="og:title" content="Captcharoo — Bot or not?" />
+        <meta name="twitter:title" content="Captcharoo — Bot or not?" />
+        <meta
+          name="description"
+          content="Lock a secret phrase behind a passcode and have suspected bots try to fill it out!"
+        />
+        <meta
+          name="og:description"
+          content="Lock a secret phrase behind a passcode and have suspected bots try to fill it out!"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@GuamHat" />
+        <meta name="twitter:site" content="@GuamHat" />
+        <meta
+          name="twitter:image"
+          content="http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg"
+        />
+        <meta name="twitter:image:alt" content="Captcharoo" />
+        <meta name="twitter:image:width" content="1200" />
+        <meta name="twitter:image:height" content="630" />
+        <meta name="twitter:domain" content="captcharoo.com" />
+        <meta
+          name="og:image"
+          content="http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg"
+        />
+        <meta name="og:image:alt" content="Captcharoo" />
+        <meta name="og:image:width" content="1200" />
+        <meta name="og:image:height" content="630" />
+        <meta name="og:url" content="https://captcharoo.com" />
+        <meta name="og:type" content="website" />
+        <meta name="og:site_name" content="Captcharoo" />
+        <meta name="og:locale" content="en_US" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -82,12 +82,6 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <title>Captcharoo</title>
-        <meta name="description" content="Bot or not?" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />{" "}
-      </Head>
       <main
         className={`${styles.main} ${FONT_OUTFIT.className} ${
           createdPhraseCode ? styles.locked : ""
